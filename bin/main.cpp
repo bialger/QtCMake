fe8a43288a6273e82025adb6ace515840f40f911
bialger <bigulov.sasha@gmail.com>
2026-02-05 15:17:16 +0300

    refactor: extract constants for timer and display settings in main.cpp

--- bin/main.cpp
+++ bin/main.cpp
@@ -5,7 +5,15 @@
 #include <QTimer>
 #include <QDateTime>
 
-int main(int argc, char* argv[]) {
+namespace {
+  static constexpr int kInterval = 1000;
+  static constexpr int kDigitCount = 8;
+  static constexpr int kWidth = 280;
+  static constexpr int kHeight = 100;
+  static constexpr const char* kFormat = "hh:mm:ss";
+} // namespace
+
+int main(int argc, char** argv) {
   if (argc > 1) {
     std::cout << "No CLI arguments allowed." << std::endl;
     return 0;
@@ -18,13 +26,13 @@ int main(int argc, char* argv[]) {
   QObject::connect(&t,
                    &QTimer::timeout,
                    [&lcd]() {
-                     lcd.display(QDateTime::currentDateTime().toString("hh:mm:ss"));
+                     lcd.display(QDateTime::currentDateTime().toString(kFormat));
                    });
-  t.start(1000);
-  lcd.setDigitCount(8);
-  lcd.display(QDateTime::currentDateTime().toString("hh:mm:ss"));
-  lcd.resize(280, 100);
+  t.start(kInterval);
+  lcd.setDigitCount(kDigitCount);
+  lcd.display(QDateTime::currentDateTime().toString(kFormat));
+  lcd.resize(kWidth, kHeight);
   lcd.show();
 
-  return a.exec();
+  return QApplication::exec();
 }
